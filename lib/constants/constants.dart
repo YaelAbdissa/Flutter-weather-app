9bb790884931f49ba7d299f8809699b9ca36ec12
Eyael <eyaelabdissa8@gmail.com>
2025-03-13 10:51:19 +0300

    initial commit

---
+++ lib/constants/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/foundation.dart' show immutable;
+
+@immutable
+class Constants {
+  static const String apiKey = 'cc1406e041904b61b6d5a94a7bcb8a39';
+  static const String apiKey2 = '18fdf4c3b0bfded0cb932ef580dd44f2';
+}
---
+++ lib/constants/navigation_destination.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+const destinations = [
+  NavigationDestination(
+    label: '',
+    icon: Icon(
+      Icons.home_outlined,
+      color: Colors.white,
+    ),
+    selectedIcon: Icon(
+      Icons.home,
+      color: Colors.white,
+    ),
+  ),
+  NavigationDestination(
+    label: '',
+    icon: Icon(
+      Icons.search_outlined,
+      color: Colors.white,
+    ),
+    selectedIcon: Icon(
+      Icons.search,
+      color: Colors.white,
+    ),
+  ),
+  NavigationDestination(
+    label: '',
+    icon: Icon(
+      Icons.wb_sunny_outlined,
+      color: Colors.white,
+    ),
+    selectedIcon: Icon(
+      Icons.wb_sunny,
+      color: Colors.white,
+    ),
+  ),
+  NavigationDestination(
+    label: '',
+    icon: Icon(
+      Icons.settings_outlined,
+      color: Colors.white,
+    ),
+    selectedIcon: Icon(
+      Icons.settings,
+      color: Colors.white,
+    ),
+  ),
+];
